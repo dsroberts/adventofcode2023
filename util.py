2ac093bc0a7c06633699efad9b812d1e706f8b58
Dale Roberts <dale.roberts987@gmail.com>
2023-12-09 06:37:45 +0000

    Add day9. Add 'submit_answer' function to util

--- util.py
+++ util.py
@@ -5,3 +5,11 @@ def get_day(day):
         s = f.read()   
     r = requests.get(f"https://adventofcode.com/2023/day/{day}/input",cookies={'session':s})
     return r.text.splitlines()
+
+def submit_answer(answer,day,part):
+    with open('session.txt','r') as f:
+        s = f.read()
+    r = requests.post(f"https://adventofcode.com/2023/day/{day}/answer",data=f"level={day}&answer={answer}",cookies={'session':s},allow_redirects=False)
+    if r.status_code == 302:
+        return "Answer already submitted or malformed data"
+    return r.text
