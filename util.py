bf6038650b72623e89045c7a8bec468801e69986
Dale Roberts <dale.roberts987@gmail.com>
2023-12-12 07:57:00 +0000

    Day 10. Engineering approach

--- util.py
+++ util.py
@@ -7,9 +7,8 @@ def get_day(day):
     return r.text.splitlines()
 
 def submit_answer(answer,day,part):
+    ### Doesn't work
     with open('session.txt','r') as f:
         s = f.read()
-    r = requests.post(f"https://adventofcode.com/2023/day/{day}/answer",data=f"level={day}&answer={answer}",cookies={'session':s},allow_redirects=False)
-    if r.status_code == 302:
-        return "Answer already submitted or malformed data"
-    return r.text
+    r = requests.post(f"https://adventofcode.com/2023/day/{day}/answer",data=f"level={day}&answer={answer}",cookies={'session':s})
+    return r
